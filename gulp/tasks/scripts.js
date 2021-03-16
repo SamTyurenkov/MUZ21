b962c70884354221d8c4bcf85156e993b2f13188
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-03-17 01:06:00 +0300

    scripts

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -5,21 +5,17 @@ let uglify = require("gulp-terser"),
     output: "./themes/asp/js/",
   };
 var babel = require("gulp-babel");
-var browserify = require("gulp-browserify");
-const include = require("gulp-include");
 
 module.exports = function () {
   $.gulp.task("scripts:site", () => {
     return $.gulp
       .src([
-        "./node_modules/@splidejs/splide/dist/js/splide.min.js",
         scriptsPATH.input + "w3schools-select.js",
         scriptsPATH.input + "*.js",
         "!" + scriptsPATH.input + "gutenberg-blocks/*.js",
         "!" + scriptsPATH.input + "authors/*.js",
       ])
       .pipe(concat("main.js"))
-      .pipe(include())
       .pipe(
         uglify({
           keep_fnames: true,
@@ -36,14 +32,12 @@ module.exports = function () {
   $.gulp.task("scripts:gutenberg", () => {
     return $.gulp
       .src([scriptsPATH.input + "gutenberg-blocks/*.js"])
-      .pipe(include())
       .pipe($.gulp.dest(scriptsPATH.output + "gutenberg-blocks/"));
   });
   $.gulp.task("scripts:authors", () => {
     return $.gulp
       .src([scriptsPATH.input + "authors/*.js"])
       .pipe(concat("authors.js"))
-      .pipe(include())
       .pipe(
         uglify({
           keep_fnames: true,
@@ -55,6 +49,11 @@ module.exports = function () {
           presets: ["@babel/preset-env"],
         })
       )
+      .pipe($.gulp.dest(scriptsPATH.output));
+  });
+  $.gulp.task("scripts:libs", () => {
+    return $.gulp
+      .src(["./node_modules/@splidejs/splide/dist/js/splide.min.js"])
       .pipe($.gulp.dest(scriptsPATH.output));
   });
 };
