c300340db107e2291027c123f0986a27ba39b1e9
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-05-16 10:53:30 +0300

    try

--- themes/asp/core/gutenberg-blocks.php
+++ themes/asp/core/gutenberg-blocks.php
@@ -1,29 +1,29 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 add_action('init', function () {
-    wp_register_script('asp-sidebanner', get_template_directory_uri() . '/js/gutenberg-blocks/sidebanner.js', array('jquery'), filemtime(get_template_directory() . '/js/gutenberg-blocks/sidebanner.js'), true);
+    wp_register_script('asp-sidebanner', get_template_directory_uri() . '/js/gutenberg-blocks/sidebanner.js', array('jquery', 'wp-editor','wp-blocks','wp-i18n','wp-element'), filemtime(get_template_directory() . '/js/gutenberg-blocks/sidebanner.js'), true);
 
     register_block_type('asp/sidebanner', [
         'editor_script' => 'asp-sidebanner',
         'render_callback' => 'blocks_render_callback',
         'attributes' => [
             'blockname' => [
-               'type' => 'string',
-               'default' => 'sidebanner'
+                'type' => 'string',
+                'default' => 'sidebanner'
             ],
-			'title' => [
-				'type' => 'string'
-			],
-			'subtitle' => [
-				'type' => 'string'
-			],
-			'image' => [
-				'type' => 'image'
-			]
-		]
+            'title' => [
+                'type' => 'string'
+            ],
+            'subtitle' => [
+                'type' => 'string'
+            ],
+            'image' => [
+                'type' => 'image'
+            ]
+        ]
     ]);
 });
 
@@ -32,10 +32,10 @@ add_action('init', function () {
 function blocks_render_callback($attr, $content)
 {
     // return the block's output here
-    
+
     $slug = $attr['blockname'];
-    if(file_exists(get_theme_file_path("/templates/gutenberg-blocks/block-{$slug}.php"))) {
-        set_query_var('block-attr',$attr);
+    if (file_exists(get_theme_file_path("/templates/gutenberg-blocks/block-{$slug}.php"))) {
+        set_query_var('block-attr', $attr);
         include get_theme_file_path("/templates/gutenberg-blocks/block-{$slug}.php");
     }
 }
