f80ea98794f046941b3892707af6677af0e635e4
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-05-16 19:58:21 +0300

    test

--- themes/asp/js/gutenberg-blocks/sidebanner.js
+++ themes/asp/js/gutenberg-blocks/sidebanner.js
@@ -2,8 +2,16 @@
 
 var __ = wp.i18n.__;
 var registerBlockType = wp.blocks.registerBlockType;
-var RichText = wp.blockEditor.RichText;
+var _wp$blockEditor = wp.blockEditor,
+    RichText = _wp$blockEditor.RichText,
+    InspectorControls = _wp$blockEditor.InspectorControls;
 var _wp$components = wp.components,
+    ToggleControl = _wp$components.ToggleControl,
+    PanelBody = _wp$components.PanelBody,
+    PanelRow = _wp$components.PanelRow,
+    CheckboxControl = _wp$components.CheckboxControl,
+    SelectControl = _wp$components.SelectControl,
+    ColorPicker = _wp$components.ColorPicker,
     TextControl = _wp$components.TextControl,
     TextareaControl = _wp$components.TextareaControl;
 registerBlockType("asp/sidebanner", {
@@ -14,15 +22,21 @@ registerBlockType("asp/sidebanner", {
   keywords: ["example", "test"],
   attributes: {
     title: {
-      type: 'string',
-      "default": 'Заголовок'
+      type: "string",
+      "default": "Заголовок"
     },
     subtitle: {
-      type: 'string',
-      "default": 'Описание'
+      type: "string",
+      "default": "Описание",
+      source: "html",
+      selector: "p"
+    },
+    bannerside: {
+      type: "string",
+      "default": "left"
     },
     postIds: {
-      type: 'array',
+      type: "array",
       "default": []
     }
   },
@@ -30,8 +44,27 @@ registerBlockType("asp/sidebanner", {
     var attributes = props.attributes,
         setAttributes = props.setAttributes;
     console.log(props);
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(RichText, {
+    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(InspectorControls, null, /*#__PURE__*/React.createElement(PanelBody, {
+      title: "\u041D\u0430\u0441\u0442\u0440\u043E\u0439\u043A\u0438 \u0431\u043B\u043E\u043A\u0430",
+      initialOpen: true
+    }, /*#__PURE__*/React.createElement(PanelRow, null, /*#__PURE__*/React.createElement(SelectControl, {
+      label: "\u0421 \u043A\u0430\u043A\u043E\u0439 \u0441\u0442\u043E\u0440\u043E\u043D\u044B \u0431\u0430\u043D\u043D\u0435\u0440?",
+      value: attributes.bannerside,
+      options: [{
+        label: "Слева",
+        value: "left"
+      }, {
+        label: "Справа",
+        value: "right"
+      }],
+      onChange: function onChange(newval) {
+        return setAttributes({
+          bannerside: newval
+        });
+      }
+    })))), /*#__PURE__*/React.createElement(RichText, {
       value: attributes.title,
+      tagName: "h2",
       onChange: function onChange(newtext) {
         return setAttributes({
           title: newtext
@@ -39,6 +72,7 @@ registerBlockType("asp/sidebanner", {
       }
     }), /*#__PURE__*/React.createElement(RichText, {
       value: attributes.subtitle,
+      tagName: "p",
       onChange: function onChange(newtext) {
         return setAttributes({
           subtitle: newtext
@@ -47,6 +81,19 @@ registerBlockType("asp/sidebanner", {
     }));
   },
   save: function save(props) {
-    return /*#__PURE__*/React.createElement("div", null, ":)");
+    var attributes = props.attributes;
+    return /*#__PURE__*/React.createElement("div", {
+      "class": "section"
+    }, /*#__PURE__*/React.createElement("div", {
+      "class": "banner {attributes.bannerside}"
+    }), /*#__PURE__*/React.createElement("div", {
+      "class": "content"
+    }, /*#__PURE__*/React.createElement(RichText.Content, {
+      tagName: "h2",
+      value: attributes.title
+    }), /*#__PURE__*/React.createElement(RichText.Content, {
+      tagName: "p",
+      value: attributes.subtitle
+    })));
   }
 });
--- themes/asp/src/js/gutenberg-blocks/sidebanner.js
+++ themes/asp/src/js/gutenberg-blocks/sidebanner.js
@@ -1,7 +1,16 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { RichText } = wp.blockEditor;
-const { TextControl, TextareaControl } = wp.components;
+const { RichText, InspectorControls } = wp.blockEditor;
+const {
+  ToggleControl,
+  PanelBody,
+  PanelRow,
+  CheckboxControl,
+  SelectControl,
+  ColorPicker,
+  TextControl,
+  TextareaControl,
+} = wp.components;
 
 registerBlockType("asp/sidebanner", {
   title: "Side Banner",
@@ -10,36 +19,71 @@ registerBlockType("asp/sidebanner", {
   description: "Show a banner and text",
   keywords: ["example", "test"],
   attributes: {
-	title: {
-		type: 'string',
-		default: 'Заголовок'
-	},
-	subtitle: {
-		type: 'string',
-		default: 'Описание'
-	},
-	postIds: {
-		type: 'array',
-		default: []
-	}
-},
-  edit: (props) => { 
-	const { attributes, setAttributes } = props;
-	console.log(props);
-	return (
-		<div>
-			<RichText 
-			value={ attributes.title }
-			onChange={(newtext) => setAttributes({ title: newtext })}
-			/>
-			<RichText 
-			value={ attributes.subtitle }
-			onChange={(newtext) => setAttributes({ subtitle: newtext })}
-			/>
-		</div>
-	);
-},
-save: (props) => { 
-	return <div>:)</div> 
-}
+    title: {
+      type: "string",
+      default: "Заголовок",
+    },
+    subtitle: {
+      type: "string",
+      default: "Описание",
+      source: "html",
+      selector: "p",
+    },
+    bannerside: {
+      type: "string",
+      default: "left",
+    },
+    postIds: {
+      type: "array",
+      default: [],
+    },
+  },
+  edit: (props) => {
+    const { attributes, setAttributes } = props;
+    console.log(props);
+    return (
+      <div>
+        <InspectorControls>
+          <PanelBody title="Настройки блока" initialOpen={true}>
+            <PanelRow>
+              <SelectControl
+                label="С какой стороны баннер?"
+                value={attributes.bannerside}
+                options={[
+                  { label: "Слева", value: "left" },
+                  { label: "Справа", value: "right" },
+                ]}
+                onChange={(newval) => setAttributes({ bannerside: newval })}
+              />
+            </PanelRow>
+          </PanelBody>
+        </InspectorControls>
+
+        <RichText
+          value={attributes.title}
+          tagName="h2"
+          onChange={(newtext) => setAttributes({ title: newtext })}
+        />
+        <RichText
+          value={attributes.subtitle}
+          tagName="p"
+          onChange={(newtext) => setAttributes({ subtitle: newtext })}
+        />
+      </div>
+    );
+  },
+  save: (props) => {
+    const { attributes } = props;
+    return (
+      <div class="section">
+	  <div class="banner {attributes.bannerside}">
+
+	  </div>
+	  <div class="content">
+        <RichText.Content tagName="h2" value={attributes.title} />
+        <RichText.Content tagName="p" value={attributes.subtitle} />
+	  </div>
+      </div>
+    );
+  },
 });
