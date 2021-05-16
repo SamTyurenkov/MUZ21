0d78bcb6ab224199a8c478ebec2786c794e39f19
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-05-16 10:21:33 +0300

    gulp

--- themes/asp/js/gutenberg-blocks/sidebanner.js
+++ themes/asp/js/gutenberg-blocks/sidebanner.js
@@ -1,18 +1,61 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const el = wp.element.createElement;
- 
-registerBlockType('asp/sidebanner', {
-	title: 'Side Banner',
-	category: 'common',
-	icon: 'smiley',
-	description: 'Show a banner and text',
-	keywords: ['example', 'test'],
-	edit: () => {  
-	//	return <div>:)</div> 
-	return null;
-	},
-	save: () => { 
-		return null;
-	}
+
+registerBlockType("asp/sidebanner", {
+  title: "Side Banner",
+  category: "common",
+  icon: "smiley",
+  description: "Show a banner and text",
+  keywords: ["example", "test"],
+  attributes: {
+    title: {
+      type: "string",
+      selector: "h2",
+    },
+    subtitle: {
+      type: "array",
+      source: "children",
+      selector: "p",
+    },
+    image: {
+      type: "image",
+      selector: "img",
+    },
+  },
+  edit: function (props) {
+    function onChangeSubtitle(content) {
+      props.attributes({ subtitle: content });
+    }
+	function onChangeTitle(content) {
+		props.attributes({ title: content });
+	  }
+	  function onChangeImage(content) {
+		props.attributes({ image: content });
+	  }
+    return (
+      <div className={props.className}>
+        <div class="gray-bg">
+		<RichText
+            tagName="h2"
+            role="textbox"
+            aria-multiline="true"
+            value={props.attributes.title}
+            onChange={onChangeTitle}
+          />
+          <RichText
+            tagName="p"
+            role="textbox"
+            aria-multiline="true"
+            value={props.attributes.subtitle}
+            onChange={onChangeSubtitle}
+          />
+        </div>
+      </div>
+    );
+    //return null;
+  },
+  save: () => {
+    return null;
+  },
 });
