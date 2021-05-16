904eed6c2634c7f3afe65f8b49668af6f41e4a4c
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-05-16 10:19:39 +0300

    ok try

--- themes/asp/templates/gutenberg-blocks/block-sidebanner.php
+++ themes/asp/templates/gutenberg-blocks/block-sidebanner.php
@@ -4,5 +4,5 @@ $attr = get_query_var('block-attr');
 <div class="section">
 <?php echo esc_html($attr['title']); ?>
 <?php echo esc_html($attr['subtitle']); ?>
-<?php echo esc_html($attr['subtitle']); ?>
+<?php echo esc_html($attr['image']); ?>
 </div>
