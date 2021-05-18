e72dddd213e9b4eac608be59d7db5d2fa91b4d36
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-05-18 20:04:52 +0300

    maybe fix?

--- themes/asp/templates/gutenberg-blocks/block-sidebanner.php
+++ themes/asp/templates/gutenberg-blocks/block-sidebanner.php
@@ -1,13 +1,15 @@
 <?php
 $attr = get_query_var('block-attr');
-$bannerclass = $attr['bannerside'] . "side sidebanner";
-$bannerimage = $attr['mediaUrl'];
+array_key_exists('bannerside',$attr) ? $bannerclass = $attr['bannerside'] . "side sidebanner" : $bannerclass = "leftside sidebanner";
+array_key_exists('mediaUrl',$attr) ? $bannerimage = $attr['mediaUrl'] : $bannerimage = '';
+array_key_exists('title',$attr) ? $title = $attr['title'] : $title = '';
+array_key_exists('subtitle',$attr) ? $title = $attr['subtitle'] : $subtitle = '';
 ?>
 
 <div class="section">
     <div class="<?php echo esc_attr($bannerclass); ?>" style="background-image:url(<?php echo esc_attr($bannerimage); ?>)"></div>
-    <div className="sidecontent">
-        <h2><?php echo esc_html($attr['title']); ?></h2>
-        <p><?php echo esc_html($attr['subtitle']); ?></p>
+    <div class="sidecontent">
+        <h2><?php echo esc_html($title); ?></h2>
+        <p><?php echo esc_html($subtitle); ?></p>
     </div>
 </div>
