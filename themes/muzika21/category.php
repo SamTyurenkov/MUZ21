0793dda4a4653b18b5353a631497ddbacb6afb0b
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-06-02 23:52:38 +0300

    clean up

--- themes/muzika21/category.php
+++ themes/muzika21/category.php
@@ -4,17 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }; 
 get_header();
 ?>
-<div class="container stickymenu transparentmenu">
-<?php get_template_part('templates/topmenu'); ?>
-</div>
 
-<div class="empty"></div>
-<?php get_template_part('templates/blogquery'); ?>
-
-
-<div style="text-align:center;padding-bottom:20px">
-<?php get_template_part('templates/sharer'); ?>
-</div>
 
 <?php get_footer(); ?>
 </body>
--- themes/muzika21/home.php
+++ themes/muzika21/home.php
@@ -4,19 +4,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }; 
 get_header();
 ?>
-<div class="container stickymenu transparentmenu" style="height:60px;">
-<?php get_template_part('templates/topmenu'); ?>
-</div>
-<div class="bannercontrols"> 
-<?php get_template_part('templates/homecats'); ?>
-</div>
-<div class="banner" style="position:relative;top:-60px;margin-bottom:-60px">
 
-</div>
 
-<?php 
-
-get_template_part('templates/homequery'); ?>
 
 <?php
 get_footer();
