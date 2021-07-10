1e3a3757024e967286b34447b0b26d7e45035773
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-07-10 17:38:58 +0300

    gallery styles

--- themes/muzyka21/templates/gutenberg-blocks/block-general-gallery.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-general-gallery.php
@@ -6,13 +6,12 @@ $gallery = get_field('gallery', $post_id);
     <div class="container">
     <div class="general-gallery_flex">
         <?php
-       $i = 0;
         foreach ($gallery as $item) : ?>
-             <figure class="general-gallery_flex_el <?= $i == 0 ? 'active' : ''; ?>">
+             <figure class="general-gallery_flex_el">
                 <img src="<?php echo esc_attr($item['url']); ?>" alt="<?php echo esc_attr($item['url']); ?>">
                 <figcaption><?php echo esc_attr($item['caption']); ?></figcaption>
             </figure>
-        <?php $i++; endforeach; ?>
+        <?php endforeach; ?>
         </div>
     </div>
 </div>
