10740b53e8d2736ebc4dc5a5843a2606a2bae2c7
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-07-11 22:12:50 +0300

    restyle places thumb

--- themes/muzyka21/templates/gutenberg-blocks/block-places-all-extended.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-places-all-extended.php
@@ -18,8 +18,9 @@
             $i = 0;
             if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
                     if ($i == 5) $i = 0; ?>
-                    <div class="places-all-extended_flex_el" >
-                        <a href="<?php echo esc_attr(get_permalink()); ?>" class="places-all-extended_flex_el_icon" style="background-image:url(<?php echo esc_attr(get_the_post_thumbnail_url(null, 'medium')); ?>)">
+                    <div class="places-all-extended_flex_el">
+                        <a href="<?php echo esc_attr(get_permalink()); ?>" class="places-all-extended_flex_el_icon">
+                            <img src="<?php echo esc_attr(get_the_post_thumbnail_url(null, 'medium')); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
                             <h3 style="background:<?php echo esc_attr($colors[$i]); ?>"><?php the_title(); ?></h3>
                         </a>
 
