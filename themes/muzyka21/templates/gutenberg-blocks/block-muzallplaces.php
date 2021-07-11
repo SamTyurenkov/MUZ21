9955adf6b0f800248b67a13783017516a73397f0
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-07-11 22:07:55 +0300

    fix places image

--- themes/muzyka21/templates/gutenberg-blocks/block-muzallplaces.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-muzallplaces.php
@@ -38,7 +38,8 @@ $posttype = get_post_type($post_id);
             if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
                     if ($i == 5) $i = 0; ?>
                     <a class="muzallplaces_flex_el" href="<?php echo esc_attr(get_permalink()); ?>">
-                        <div class="muzallplaces_flex_el_icon" style="background-image:url(<?php echo esc_attr(get_the_post_thumbnail_url(null, 'medium')); ?>)">
+                        <div class="muzallplaces_flex_el_icon">
+                            <img src="<?php echo esc_attr(get_the_post_thumbnail_url(null, 'medium')); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
                             <h3 style="background:<?php echo esc_attr($colors[$i]); ?>"><?php the_title(); ?></h3>
                         </div>
                     </a>
