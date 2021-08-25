55c1ef9a2a03fb823ca5a518fb9112c56a109292
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-08-25 21:16:44 +0300

    fix cf autocomplete

--- themes/muzyka21/templates/gutenberg-blocks/block-general-contact-form.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-general-contact-form.php
@@ -17,15 +17,15 @@ $translations = get_field('settings', 'option');
                 <div class="general-contact-form_flex_text_input_holder">
                     <span class="input_container input_large">
                         <label><?php echo esc_html($translations['label_email']); ?></label>
-                        <input required type="email" placeholder="your@email.domain" value="<?php echo esc_attr(get_the_author_meta('email', get_current_user_id())); ?>"></input>
+                        <input required type="email" placeholder="your@email.domain" value="<?php if(get_current_user_id() > 0) echo esc_attr(get_the_author_meta('email', get_current_user_id())); ?>"></input>
                     </span>
                     <span class="input_container input_large">
                         <label><?php echo esc_html($translations['label_display_name']); ?></label>
-                        <input class="general-contact-form_username" required type="text" placeholder="Имя или ник" value="<?php echo esc_attr(get_the_author_meta('display_name', get_current_user_id())); ?>"></input>
+                        <input class="general-contact-form_username" required type="text" placeholder="Имя или ник" value="<?php if(get_current_user_id() > 0) echo esc_attr(get_the_author_meta('display_name', get_current_user_id())); ?>"></input>
                     </span>
                     <span class="input_container input_large">
                         <label><?php echo esc_html($translations['label_phone']); ?></label>
-                        <input class="general-contact-form_userphone" required type="text" placeholder="+7...." value="<?php echo esc_attr(get_the_author_meta('user_phone', get_current_user_id())); ?>"></input>
+                        <input class="general-contact-form_userphone" required type="text" placeholder="+7...." value="<?php if(get_current_user_id() > 0) echo esc_attr(get_the_author_meta('user_phone', get_current_user_id())); ?>"></input>
                     </span>
                     <input class="general-contact-form_page" type="page" hidden value="<?php echo esc_attr(get_permalink()); ?>"></input>
                     <input type="submit" class="button" value="<?php echo esc_attr(get_field('contact_form_submit_text', 'option')); ?>">
