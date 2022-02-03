a23f1e29388f9bf1509277a11e725d3071c43a3b
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2022-02-03 22:26:52 +0300

    wave end 2 options

--- themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
@@ -5,6 +5,8 @@
     if($class == 'start') : ?>
     <img class="wavestart" src="<?php echo get_template_directory_uri() . '/images/wave2start.svg'; ?>">
     <?php elseif ($class == 'end') : ?>
+    <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/waveend.svg'; ?>">
+    <?php elseif ($class == 'end2') : ?>
     <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/wave2end.svg'; ?>">
     <?php endif; ?>
 </div>
