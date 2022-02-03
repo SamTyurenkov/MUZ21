ba2ec91d5f29ddd562e0e090835655fa24c34d48
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2022-02-03 22:23:50 +0300

    fix block

--- themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
@@ -4,7 +4,7 @@
 
     if($class == 'start') : ?>
     <img class="wavestart" src="<?php echo get_template_directory_uri() . '/images/wave2start.svg'; ?>">
-    <?php else if ($class == 'end') : ?>
+    <?php elseif ($class == 'end') : ?>
     <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/wave2end.svg'; ?>">
     <?php endif; ?>
 </div>
