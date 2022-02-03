33d30f2837f4ef26b17a2b9d311e89426aa9c5aa
Sam Tyurenkov <sam.tyurenkov@gmail.com>
2022-02-03 22:30:55 +0300

    pinkier

--- themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
+++ themes/muzyka21/templates/gutenberg-blocks/block-general-wavefix.php
@@ -5,7 +5,7 @@
     if($class == 'start') : ?>
     <img class="wavestart" src="<?php echo get_template_directory_uri() . '/images/wave2start.svg'; ?>">
     <?php elseif ($class == 'end') : ?>
-    <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/waveend.svg'; ?>">
+    <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/waveend_pinkier.svg'; ?>">
     <?php elseif ($class == 'end2') : ?>
     <img class="wavesend" src="<?php echo get_template_directory_uri() . '/images/wave2end.svg'; ?>">
     <?php endif; ?>
